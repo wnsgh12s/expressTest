4542808f1041f694bc503f762cc56dae27a8ee8e
junho lee <junho1273@gmail.com>
2022-08-03 23:02:00 +0900

    react connect express

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.listen(8080,()=>{
   console.log('8080포트로 접속완료')
 })
 
-app.get('/',function(요청,응답){
-  응답.send('안녕..?')
-})
+app.use(express.static(path.join(__dirname, 'my-app/build')));
+
+app.get('/', function (요청, 응답) {
+  응답.sendFile(path.join(__dirname, '/my-app/build/index.html'));
+});
+
+//라우팅 리액트에게 넘기기
+app.get('*', function (요청, 응답) {
+  응답.sendFile(path.join(__dirname, '/my-app/build/index.html'));
+});
